9e1e9047c61f04f7efdadd6e73361faa9630f879
Hamza Sajid <hamzasajid4341@gmail.com>
2025-02-07 08:21:42 +0500

    Fix

--- to-do-list/Script/myscript.js
+++ to-do-list/Script/myscript.js
@@ -1,44 +1,81 @@
+// Get reference to the input box element
 const inputBox = document.getElementById("input-box");
+
+// Get reference to the unordered list container
 const listContainer = document.getElementById("list-container");
 
-function addTask(){
-    if(inputBox.value === ''){
+// Function to add a new task
+function addTask() {
+    // Check if the input box is empty
+    if (inputBox.value === '') {
         alert("You must write something...!");
-    }
-    else{
+    } else {
+        // Create a new list item (li) element
         let li = document.createElement("li");
-        li.innerHTML=inputBox.value;
+        
+        // Set the innerHTML of the list item to the value of the input box
+        li.innerHTML = inputBox.value;
+
+        // Append the new list item to the list container
         listContainer.appendChild(li);
+
+        // Create a new span element for the delete button
         let span = document.createElement("span");
+        
+        // Add the "×" symbol to the span (used as a delete button)
         span.innerHTML = "\u00d7";
+        
+        // Append the span to the list item
         li.appendChild(span);
     }
-    inputBox.value="";
+    
+    // Clear the input box for the next task
+    inputBox.value = "";
+    
+    // Save the updated task list to local storage
     saveData();
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName ==="LI"){
+// Add a click event listener to the list container
+listContainer.addEventListener("click", function(e) {
+    // Check if the clicked element is a list item (LI)
+    if (e.target.tagName === "LI") {
+        // Toggle the "checked" class to mark/unmark the task
         e.target.classList.toggle("checked");
+        
+        // Save the updated task list to local storage
         saveData();
     }
-    else if(e.target.tagName ==="SPAN"){
+    // Check if the clicked element is a span (delete button)
+    else if (e.target.tagName === "SPAN") {
+        // Remove the parent list item (task) of the clicked span
         e.target.parentElement.remove();
+        
+        // Save the updated task list to local storage
         saveData();
     }
-},false);
+}, false);
 
-function saveData(){
-    localStorage.setItem("data",listContainer.innerHTML);
+// Function to save the task list to local storage
+function saveData() {
+    // Save the innerHTML of the list container to local storage under the key "data"
+    localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function showTask(){
+// Function to show the saved tasks from local storage when the page loads
+function showTask() {
+    // Retrieve the task list from local storage and set it as the innerHTML of the list container
     listContainer.innerHTML = localStorage.getItem("data");
 }
+
+// Call the showTask function to display the saved tasks on page load
 showTask();
 
-inputBox.addEventListener('keyup', (e)=>{
-    if(e.keyCode === 13){
+// Add an event listener to the input box to detect when the "Enter" key is pressed
+inputBox.addEventListener('keyup', (e) => {
+    // Check if the key pressed is "Enter" (key code 13)
+    if (e.keyCode === 13) {
+        // Call the addTask function to add the task
         addTask();
     }
 });
