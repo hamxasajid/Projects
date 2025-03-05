3ff802de0d52e931395bc55a7fe9c495c4cf64aa
Hamza Sajid <hamzasajid4341@gmail.com>
2025-03-05 18:49:07 +0500

    Work About PWA

---
+++ to-do-list/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "listify-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./Style/style.css",
+  "./Script/myscript.js",
+  "./assets/list.png",
+];
+
+// Install the service worker and cache the necessary files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch the resources from cache if available, otherwise request from the network
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate the service worker and delete old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
